848196d99cfd4854dcf244afbc82da6542e48c45
Alberto Rodriguez <arodriguez@stratio.com>
2015-08-04 09:24:59 +0200

    Add OAUTH token info for private repos

--- src/main/scala/GithubApiCaller.scala
+++ src/main/scala/GithubApiCaller.scala
@@ -9,13 +9,18 @@ import scala.concurrent.duration._
 
 class GithubApiCaller {
 
+  //If you want to fetch info from private repos you should provide your oauth token
+  //TODO: pass-in this guy through command line or fetch it from a config file
+  val accessToken = ""
+
   def contributionsStats(repoName: String): String = {
     val clientConfig = new DefaultWSClientConfig()
     val asyncClient = new NingAsyncHttpClientConfigBuilder(clientConfig).build()
     val builder = new AsyncHttpClientConfig.Builder(asyncClient)
     val secureDefaultsWithSpecificOptions = builder.build()
     implicit val implicitClient = new NingWSClient(secureDefaultsWithSpecificOptions)
-    val response = WS.clientUrl(s"https://api.github.com/repos/stratio/$repoName/stats/contributors").get()
+    val addAccessToken = if (accessToken.isEmpty) "" else accessToken
+    val response = WS.clientUrl(s"https://api.github.com/repos/stratio/$repoName/stats/contributors$addAccessToken").get()
     println(s"About to call gihub API to get contributors for $repoName...")
     val result = Await.result(response, 10 seconds)
     val stats = result.status match {
